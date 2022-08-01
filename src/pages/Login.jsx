289e2b5e96a7b161985bf7791fdc759cc395f540
Guyddo <guyddogl@gmail.com>
2022-08-01 16:32:42 -0300

    Req 2

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,62 +1,83 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Loading from '../components/Loading';
+import { createUser } from '../services/userAPI';
 
-class Search extends Component {
+class Login extends Component {
   state = {
     isEnterButtonDisabled: true,
     userName: '',
+    isLoading: false,
   };
 
-  validateName = ({ userName } = this.state) => {
-    const minInputLength = 2;
+  validateInputName = ({ userName } = this.state) => {
+    const minInputLength = 3;
     return userName.length < minInputLength;
   }
 
   onInputChange = ({ target }) => {
-    const { name } = target;
-    const { userName } = this.state;
-    const minInputLength = 2;
-    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const { name, value } = target;
     this.setState({ [name]: value }, () => {
-      this.setState({ isEnterButtonDisabled: userName.length < minInputLength });
+      this.setState({ isEnterButtonDisabled: this.validateInputName() });
+    });
+  }
+
+  handleClick = () => {
+    this.setState({ isLoading: true }, async () => {
+      const { userName } = this.state;
+      const { history } = this.props;
+      await createUser({ name: userName });
+      history.push('/search');
     });
   }
 
   render() {
-    const { isEnterButtonDisabled } = this.state;
+    const { isEnterButtonDisabled, isLoading } = this.state;
     return (
       <div className="container" data-testid="page-login">
-        <span className="fs-4 title">
-          TrybeTunes
-        </span>
-        <form className="my-4">
-          <div className="input-group mb-3">
-            <span
-              className="input-group-text username"
-            >
-              <i className="far fa-user-circle fa-lg" />
-            </span>
-            <input
-              type="text"
-              name="userName"
-              className="form-control input"
-              data-testid="login-name-input"
-              placeholder="Type your username"
-              onChange={ (e) => this.onInputChange(e) }
-            />
-          </div>
-          <button
-            type="submit"
-            className="btn btn-success submit mt-2"
-            data-testid="login-submit-button"
-            disabled={ isEnterButtonDisabled }
-            onClick={ (userName) => createUser({ name: userName }) }
-          >
-            Enter
-          </button>
-        </form>
+        {isLoading ? <Loading />
+          : (
+            <div>
+              <span className="fs-4 title">
+                TrybeTunes
+              </span>
+              <form className="my-4">
+                <div className="input-group mb-3">
+                  <span
+                    className="input-group-text username"
+                  >
+                    <i className="far fa-user-circle fa-lg" />
+                  </span>
+                  <input
+                    type="text"
+                    name="userName"
+                    className="form-control input"
+                    data-testid="login-name-input"
+                    placeholder="Type your username"
+                    onChange={ (e) => this.onInputChange(e) }
+                  />
+                </div>
+                <button
+                  type="submit"
+                  className="btn btn-success submit mt-2"
+                  data-testid="login-submit-button"
+                  disabled={ isEnterButtonDisabled }
+                  onClick={ this.handleClick }
+                >
+                  Enter
+                </button>
+              </form>
+            </div>
+          )}
       </div>
     );
   }
 }
 
-export default Search;
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
+
+export default Login;
