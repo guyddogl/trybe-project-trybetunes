fd56b8a451198bbd68f8fe7240797fc661388060
Guyddo <guyddogl@gmail.com>
2022-08-01 17:10:16 -0300

    Req 3

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,36 @@
 import React, { Component } from 'react';
+import Loading from './Loading';
+import { getUser } from '../services/userAPI';
 
 class Header extends Component {
+  state = {
+    isLoading: false,
+    userName: '',
+  };
+
+  componentDidMount() {
+    this.setState({ isLoading: true }, async () => {
+      const dataUser = await getUser();
+      this.setState({
+        userName: dataUser.name,
+        isLoading: false,
+      });
+    });
+  }
+
   render() {
+    const { isLoading, userName } = this.state;
     return (
-      <header
-        data-testid="header-component"
-      >
-        teste
-      </header>
+      <div>
+        {isLoading ? <Loading />
+          : (
+            <header
+              data-testid="header-component"
+            >
+              <span data-testid="header-user-name">{userName}</span>
+            </header>
+          )}
+      </div>
     );
   }
 }
