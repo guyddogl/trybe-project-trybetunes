481aa801fe0aa874a0d2acbb886270fa10537ac3
Guyddo <guyddogl@gmail.com>
2022-08-01 17:35:05 -0300

    Create search input

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,12 +2,53 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 class Search extends Component {
+  state = {
+    isSearchButtonDisabled: true,
+  };
+
+  validateInputName = ({ userName } = this.state) => {
+    const minInputLength = 2;
+    return userName.length < minInputLength;
+  }
+
+  onInputChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({ [name]: value }, () => {
+      this.setState({ isSearchButtonDisabled: this.validateInputName() });
+    });
+  }
+
+  handleSearch = () => {
+    console.log('teste');
+  }
+
   render() {
+    const { isSearchButtonDisabled } = this.state;
     return (
       <>
         <Header />
         <div data-testid="page-search">
-          Search
+          <form className="my-4">
+            <div className="input-group mb-3">
+              <input
+                type="text"
+                name="userName"
+                className="form-control input"
+                data-testid="search-artist-input"
+                placeholder="Type your username"
+                onChange={ (e) => this.onInputChange(e) }
+              />
+            </div>
+            <button
+              type="submit"
+              className="btn btn-success submit mt-2"
+              data-testid="search-artist-button"
+              disabled={ isSearchButtonDisabled }
+              onClick={ this.handleSearch }
+            >
+              Enter
+            </button>
+          </form>
         </div>
       </>
     );
