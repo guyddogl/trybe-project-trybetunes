ef0d4538e1587be4e75c2911cc49d6ac51c3f71e
Guyddo <guyddogl@gmail.com>
2022-08-01 16:31:19 -0300

    Create Header and Loading components

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <header
+        data-testid="header-component"
+      >
+        teste
+      </header>
+    );
+  }
+}
+
+export default Header;
---
+++ src/components/Loading.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <span>Carregando...</span>
+    );
+  }
+}
+
+export default Header;
